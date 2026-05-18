dafe53e1c1d638ec483c2adc4ca83f6d0bd5cdd5
Andres Cepeda <120003994+anduchocepeda93@users.noreply.github.com>
2026-05-17 21:39:50 -0500

    Add files via upload

---
+++ python_with_dalto/datos-simples.py
@@ -0,0 +1,21 @@
+a = 5
+b = 6
+c = a + b
+print(c)
+d = "Hola"
+print(d)
+e = 2.2
+print(e)
+t = True
+f = False
+
+print(t,f)
+"ANdres"
+
+"""Andres
+    Cepeda
+    Aveiga"""
+
+#este es un comentario
+
+
---
+++ python_with_dalto/metodos_diccionarios.py
@@ -0,0 +1,13 @@
+diccionario = {
+    "nombre" : "Andres",
+    "apellido" : "Cepeda",
+    "edad" : 30,
+    "es_casado" : True
+}
+
+#claves = diccionario.keys()
+#get = diccionario.get("edad")
+#diccionario.clear()
+diccionario.pop("edad")
+
+print(diccionario)
